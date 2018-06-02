8e978fc525a08b2484ac5d0f8437ea86123fd589
Talrandel <makarovevgeniy7a@gmail.com>
2018-06-02 21:29:56 +0300

    12 занятие.

---
+++ Lessons/Lesson_12/A.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Lesson_12
+{
+    class A
+    {
+        public byte Byte;
+        public virtual void Do()
+        {
+            Console.WriteLine();
+            Console.WriteLine("A.Do - virtual");
+        }
+    }
+
+    class B : A
+    {
+        public int Int;
+        public override void Do()
+        {
+            base.Do();
+            Console.WriteLine("B.Do - override");
+        }
+
+        /// <summary>
+        /// Оператор неявного приведения типа int к типу B -> B b =(B)int;
+        /// </summary>
+        /// <param name="i"></param>
+        public static implicit operator B(int i)
+        {
+            return new B { Int = i };
+        }
+
+        /// <summary>
+        /// Оператор неявного приведения типа F к типу B -> B b = (B)F;
+        /// </summary>
+        /// <param name="f"></param>
+        public static implicit operator B(F f)
+        {
+            return new B { Int = (int)f.Long };
+        }
+
+        /// <summary>
+        /// Оператор явного приведения типа B к типу int -> int x = new B() { Int = 5};
+        /// </summary>
+        /// <param name="b"></param>
+        public static explicit operator int(B b)
+        {
+            return b.Int;
+        }
+
+        /// <summary>
+        /// Оператор явного приведения типа B к типу string -> string s = new B();
+        /// </summary>
+        /// <param name="b"></param>
+        public static explicit operator string(B b)
+        {
+            return "Тип B всегда приводится к такой строке!";
+        }
+    }
+
+    class C : B
+    {
+        public new virtual void Do()
+        {
+            Console.WriteLine();
+            Console.WriteLine("C.Do - new virtual");
+        }
+    }
+
+    sealed class D : C
+    {
+
+        public override void Do()
+        {
+            base.Do();
+            Console.WriteLine("D.Do - override");
+        }
+    }
+
+    class F
+    {
+        public long Long;
+    }
+}
---
+++ Lessons/Lesson_12/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Перекрытие методов
+            //A a = new A();
+            //A b = new B();
+            //C c = new C();
+            //A d = new D();
+
+            //a.Do();
+            //b.Do();
+            //c.Do();
+            //d.Do(); 
+            #endregion
+
+            // Явное преобразование
+            //c = (C)d;
+
+            //int x = (int)d;
+
+            //long l = 157;
+            //object o = l;
+            //int i = (int)o;
+
+            B b = new B() { Int = 5 };
+
+            int x = (int)b;
+            Console.WriteLine(x);
+
+            string s1 = (string)b;
+            Console.WriteLine(s1);
+
+            string s2 = (string)new B();
+            Console.WriteLine(s2);
+
+            B newB = 157;
+            Console.WriteLine(newB.Int);
+
+
+            Console.ReadLine();
+        }
+    }
+}
